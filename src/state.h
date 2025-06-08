910a4060b4fd6b5b9a181390eac9cf9d55d81974
alexbataille <alex.bataille@ulb.be>
2025-06-08 22:37:33 +0200

    struct of cellstate created

---
+++ src/state.h
@@ -0,0 +1,14 @@
+#ifndef _CELL_STATE_H
+#define _CELL_STATE_H
+
+#define P 10
+
+struct CellState {
+  int x;          // X coordinate of the cell
+  int y;          // Y coordinate of the cell
+  float value;   // Value of the cell
+} CellState;
+
+
+
+#endif // _CELL_STATE_H
